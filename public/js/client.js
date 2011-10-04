a871abfe689177c0f2b8793b3860a597f7164cfd
Ryan Greenberg <ryangreenberg@gmail.com>
2011-10-04 09:17:13 -0700

    adds initial support for client display of new stories

--- public/js/client.js
+++ public/js/client.js
@@ -17,7 +17,8 @@ var iter8 = {
 
   // Populated from server
   users: {},
-  currentStory: null
+  currentStory: null,
+  state: 'votingOnStory' // Should be populated when new story starts
 };
 
 iter8.send = function(eventName, msg, fn) {
@@ -34,10 +35,15 @@ iter8.send = function(eventName, msg, fn) {
 
 iter8.events = {
   userList: function(msg){
-    // TODO replace with underscore when I have access to the Internet
     iter8.users = iter8.deserializer.deserialize(msg);
     iter8.ui.updateUsers(iter8.users);
     iter8.debug.logEvent(null, {eventName: 'userList', msg: msg});
+  },
+
+  newStory: function(msg) {
+    iter8.currentStory = iter8.deserializer.deserialize(msg);
+    iter8.ui.showNewStory(iter8.currentStory);
+    iter8.debug.logEvent(null, {eventName: 'newStory', msg: msg});
   },
 
   voteList: function(msg){
@@ -46,6 +52,7 @@ iter8.events = {
   },
 
   votingComplete: function(msg){
+    iter8.state = 'viewingVotingResults';
     iter8.ui.hideVotingControls();
     iter8.ui.showVotingResults(msg);
     iter8.debug.logEvent(null, {eventName: 'showVotingResults', msg: msg});
@@ -72,6 +79,23 @@ iter8.ui = {
     iter8.send('closeVoting', {}, iter8.events.votingComplete);
   },
 
+  createNewStory: function() {
+    if (iter8.state == 'votingOnStory') {
+      var abandon = confirm("Connected users are currently pointing a story. Are you sure you want to abandon this story and create a new one?");
+      if (!abandon) {
+        return;
+      }
+    }
+
+    var storyName = prompt("Enter a name for the new story:");
+    iter8.send('newStory', {name: storyName}, iter8.events.newStory);
+  },
+
+  showNewStory: function(story) {
+    $('.voting').show();
+    $('.story-name').text(story.name);
+  },
+
   updateUsers: function(users) {
     var userIds = users.map(function(u){ return u.id; });
     // Remove existing users who are no longer connected
@@ -94,7 +118,7 @@ iter8.ui = {
   },
 
   updateVotes: function(votesById) {
-    // Add new users who have recently connected
+    // TODO: Add new users who have recently connected?
     for(var id in votesById) {
       var user = $('.user[data-user-id=' + id + ']');
       if (votesById[id] !== null) {
@@ -122,6 +146,10 @@ iter8.ui = {
     $('.average').text(iter8.util.round(iter8.util.average(votes), 1));
     $('.median').text(iter8.util.round(iter8.util.median(votes), 1));
     iter8.ui.displayDistribution(votes);
+  },
+
+  hideVotingResults: function() {
+    $('.results').hide();
   },
 
   displayDistribution: function(votes) {
@@ -203,10 +231,12 @@ $(function() {
   // UI
   $('.point-button').click(iter8.ui.toggleVote);
   $('.user.me').live('click', iter8.ui.changeName);
+  $('#new-story').click(iter8.ui.createNewStory);
   $('#close-voting').click(iter8.ui.closeVoting);
 
   // MESSAGES
   iter8.socket.on('userList', iter8.events.userList);
+  iter8.socket.on('newStory', iter8.events.newStory);
   iter8.socket.on('voteList', iter8.events.voteList);
   iter8.socket.on('votingComplete', iter8.events.votingComplete);
 
